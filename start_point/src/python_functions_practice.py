874b07c58e0a4f0dd1e9775613e3d0aa522bba86
Bela <belahamid@gmail.com>
2020-11-18 20:28:49 +0000

    initial commit

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,52 @@
+def return_10():
+    return 10 
+
+def add(num1,num2):
+    return num1+num2
+
+def subtract(num1, num2):
+    return num1 - num2 
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 /num2
+
+def length_of_string(teststring):
+    return len(teststring)
+
+def add_string_as_number(num1, num2):
+    return int(num1)+int(num2)
+    
+def test_number_to_full_name__month_1(index):
+    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
+    return month[index]
+
+def test_number_to_full_name__month_1(index):
+    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
+    return month[index]
+
+test_number_to_full_name__month_1(2)
+   
+   #IT WORKS
+
+    
+
+
+
+
+def join_string(string1, string2):
+    return string1 + string2
+
+
+
+
+
+
+def test_reverse_string(string):
+      return string[::-1]
+mytxtbackwards = test_reverse_string("God Help us!")
+print(mytxtbackwards)
+
+
