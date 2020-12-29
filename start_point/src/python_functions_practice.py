760892eafdbbad5b78372aba9738a21bb0d0715e
Bela <belahamid@gmail.com>
2020-12-29 00:52:46 +0000

    test passed for number_to_short_month_name()

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -1,11 +1,11 @@
 def return_10():
-    return 10 
+    return 10
 
-def add(num1,num2):
-    return num1+num2
+def add(num1, num2):
+    return num1 + num2
 
 def subtract(num1, num2):
-    return num1 - num2 
+    return num1 - num2
 
 def multiply(num1, num2):
     return num1 * num2
@@ -16,37 +16,32 @@ def divide(num1, num2):
 def length_of_string(teststring):
     return len(teststring)
 
-def add_string_as_number(num1, num2):
-    return int(num1)+int(num2)
-    
-def test_number_to_full_name__month_1(index):
-    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
-    return month[index]
-
-def test_number_to_full_name__month_1(index):
-    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
-    return month[index]
-
-test_number_to_full_name__month_1(2)
-   
-   #IT WORKS
-
-    
-
-
-
-
 def join_string(string1, string2):
     return string1 + string2
 
+def add_string_as_number(num1, num2):
+    return int(num1)+int(num2)
+
+def number_to_full_month_name(index):
+    month = ['none', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    return month[index]
 
 
+def number_to_short_month_name(index):
+    months = ['none', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    short_month_name = []
+    for month in months:
+        split_month = month[0:3]
+        short_month_name.append(split_month)
+    # print(short_month_name[index])
+    return short_month_name[index]
+
+number_to_short_month_name(1)
 
 
-
-def test_reverse_string(string):
-      return string[::-1]
-mytxtbackwards = test_reverse_string("God Help us!")
-print(mytxtbackwards)
+# def test_reverse_string(string):
+#       return string[::-1]
+# mytxtbackwards = test_reverse_string("God Help us!")
+# print(mytxtbackwards)
 
 
